5449b50d56954008aeb79a3ed8d73d7aa8256717
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2025-03-11 02:14:04 -0700

    Implemented Options Parser
    Implemented Options Parser. It can handle a string path option and a boolean. The path option should be to the file to be interpreted and the boolean specifies if the file at path is in RISC-V Assembly or machine code.

--- include/CommandLineArgumentParser.h
+++ include/CommandLineArgumentParser.h
@@ -2,11 +2,22 @@
 
 #include "cxxopts.hpp"
 
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 class CommandLineArgumentParser {
 public:
     CommandLineArgumentParser(int argc, char** argv);
-
+    CommandLineOptions parse();
+    
 private:
     int m_argc;
     char** m_argv;
+    cxxopts::Options m_options;
+};
+
+struct CommandLineOptions {
+    fs::path programPath;
+    bool isMachineCode;
 };
---
+++ src/CommandLineArgumentParser.cpp
@@ -0,0 +1,25 @@
+#include "CommandLineArgumentParser.h"
+#include "Utility.h"
+
+#include "cxxopts.hpp"
+
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+CommandLineArgumentParser::CommandLineArgumentParser(int argc, char** argv) : m_argc(argc), m_argv(argv), m_options(m_argv[0]) {
+    m_options.add_options()
+        ("f,file", "File", cxxopts::value<std::string>())
+        ("b,binary", "Machine Code", cxxopts::value<bool>()->default_value(false));
+}
+
+
+CommandLineOptions CommandLineArgumentParser::parse() {
+    auto result = m_options.parse(m_argc, m_argv);
+
+    fs::path filePath = result["file"].as<std::string>();
+    bool isMachineCode = result["binary"].as<bool>();
+
+    return {filePath, isMachineCode};
+}
