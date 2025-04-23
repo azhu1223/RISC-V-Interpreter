f5bb6fa373175833466ba707ce508819b03b471e
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2025-04-23 02:19:12 -0700

    Added CommandLineArgumentParser Tests
    Added some basic tests for the CommandLineArgumentParser class.

--- tests/CommandLineArgumentParserTest.cpp
+++ tests/CommandLineArgumentParserTest.cpp
@@ -3,3 +3,47 @@
 #include "CommandLineArgumentParser.h"
 #include "Utility.h"
 
+class CommandLineArgumentParserTest : public ::testing::Test {
+protected:
+    std::string m_pathMarker = "-f";
+    std::string m_binaryMarker = "-b";
+    std::string m_helpMarker = "-h";
+    std::string m_path = "path/file";
+    std::string m_programName = "program";
+};
+
+TEST_F(CommandLineArgumentParserTest, ValidArguments) {
+    int argc = 5;
+    char* argv[5] = {m_programName.data(), m_pathMarker.data(), m_path.data(), m_binaryMarker.data(), m_helpMarker.data()};
+
+    CommandLineArgumentParser parser(argc, argv);
+
+    Result<CommandLineOptions> result = parser.parse();
+
+    EXPECT_TRUE(result.first);
+
+    CommandLineOptions options = result.second;
+
+    EXPECT_TRUE(options.isMachineCode);
+    EXPECT_TRUE(options.isHelp);
+    EXPECT_EQ(options.programPath, m_path);
+}
+
+TEST_F(CommandLineArgumentParserTest, HelpString) {
+    int argc = 5;
+    char* argv[5] = {m_programName.data(), m_pathMarker.data(), m_path.data(), m_binaryMarker.data(), m_helpMarker.data()};
+
+    CommandLineArgumentParser parser(argc, argv);
+    std::string helpString = parser.getHelpString();
+
+    std::cout << helpString;
+}
+
+TEST_F(CommandLineArgumentParserTest, MissingPath) {
+    char * argv[] = {m_programName.data()};
+    CommandLineArgumentParser parser(1, argv);
+
+    Result<CommandLineOptions> result = parser.parse();
+
+    EXPECT_FALSE(result.first);
+}
