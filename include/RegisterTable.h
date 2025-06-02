aac918620128527afbb3ccf714bcdf60c0506a0c
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2025-06-02 06:10:38 -0700

    Implemented RegisterTable API
    Implemented RegisterTable API. Changed set and get to use uint32_t type values instead of ints. Also, changed register numbers to uint8_ts.

--- include/RegisterTable.h
+++ include/RegisterTable.h
@@ -11,10 +11,11 @@ using RegisterArray = std::array<uint32_t, 32>;
 class RegisterTable {
 public:
     RegisterTable(u_ptr<RegisterArray> regArray);
-    bool set(int reg, uint32_t val);
-    int get(int reg) const;
+    bool set(uint8_t reg, uint32_t val);
+    uint32_t get(uint8_t reg) const;
 private:
     u_ptr<RegisterArray> m_regArray;
+    int m_numRegisters;
 };
 
 #endif
--- src/RegisterTable.cpp
+++ src/RegisterTable.cpp
@@ -1,2 +1,29 @@
 #include "RegisterTable.h"
 #include "Utility.h"
+
+#include "Utility.h"
+
+#include <memory>
+#include <array>
+#include <cstdint>
+
+RegisterTable::RegisterTable(u_ptr<RegisterArray> regArray) : m_regArray(std::move(regArray)), m_numRegisters(m_regArray->size()) {
+    (*m_regArray)[0] = 0;
+}
+
+bool RegisterTable::set(uint8_t reg, uint32_t val) {
+    if (reg >= m_numRegisters) {
+        return false;
+    }
+
+    // x0 should stay 0
+    else if (reg != 0) {
+        (*m_regArray)[reg] = val;
+    }
+
+    return true;
+}
+
+uint32_t RegisterTable::get(uint8_t reg) const {
+    return (*m_regArray)[reg];
+}
