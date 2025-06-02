db1065187a290e6827b37a309f39100b7cffce9d
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2025-06-02 06:31:10 -0700

    Create RegisterTableTest.cpp
    Added basic tests for RegisterTable.

---
+++ tests/RegisterTableTest.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "RegisterTable.h"
+#include "Utility.h"
+
+class RegisterTableTest : public ::testing::Test {
+protected:
+    RegisterTableTest() : m_rt(u_ptr<RegisterArray>(new RegisterArray)) {};
+    RegisterTable m_rt;
+
+    virtual void SetUp() override {
+        for (int i = 0; i < 31; i++) {
+            EXPECT_TRUE(m_rt.set(i, i + 1));
+        }
+    };
+};
+
+TEST_F(RegisterTableTest, Set) {
+    for (int i = 1; i < 31; i++) {
+        EXPECT_TRUE(m_rt.set(i, i + 1));
+    }
+
+    for (int i = 32; i < 40; i++) {
+        EXPECT_FALSE(m_rt.set(i, i + 1));
+    }
+}
+
+TEST_F(RegisterTableTest, Get) {
+    EXPECT_EQ(m_rt.get(0), 0);
+
+    for (int i = 1; i < 31; i++) {
+        EXPECT_EQ(m_rt.get(i), i + 1);
+    }
+}
