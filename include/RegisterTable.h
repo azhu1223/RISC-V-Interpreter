8c99d07cde5af1dbb2660dbdaf73b73af2b01b76
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2025-06-02 03:53:30 -0700

    Update RegisterTable.h
    Defined the API of a RegisterTable. They have a getter and setter that allows you to set and get registers (held in a std Array) individually. The Array is provided via dependency injection at construction.

--- include/RegisterTable.h
+++ include/RegisterTable.h
@@ -3,10 +3,18 @@
 
 #include "Utility.h"
 
+#include <array>
+#include <cstdint>
+
+using RegisterArray = std::array<uint32_t, 32>;
+
 class RegisterTable {
 public:
-
+    RegisterTable(u_ptr<RegisterArray> regArray);
+    bool set(int reg, uint32_t val);
+    int get(int reg) const;
 private:
+    u_ptr<RegisterArray> m_regArray;
 };
 
 #endif
